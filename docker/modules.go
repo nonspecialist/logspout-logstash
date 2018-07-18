6b3b9c00d4a1a8b04e19ff1e407efbc84af730a8
Colin Panisset <colin.panisset@cevo.com.au>
2018-07-18 17:00:12 +1000

    Should not have two packages in same dir

--- docker/modules.go
+++ docker/modules.go
@@ -3,5 +3,5 @@ package main
 import (
 	_ "github.com/gliderlabs/logspout/transports/tcp"
 	_ "github.com/gliderlabs/logspout/transports/udp"
-	_ "github.com/nonspecialst/logspout-logstash-k8s"
+	_ "github.com/nonspecialist/logspout-logstash-k8s"
 )
