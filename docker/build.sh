2789315fa64a52ce61ac75418fbbf6651ceb6c5b
Colin Panisset <colin.panisset@cevo.com.au>
2018-07-23 15:13:25 +1000

    Build debugging

--- docker/build.sh
+++ docker/build.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 set -e
+env
 apk add --update go git mercurial build-base ca-certificates
 mkdir -p /go/src/github.com/gliderlabs
 cp -r /src /go/src/github.com/gliderlabs/logspout
 cd /go/src/github.com/gliderlabs/logspout
 export GOPATH=/go
 echo Cleaning up old versions
-rm -rf /go/src/github.com/nonspecialist
+rm -rvf /go/src/github.com/nonspecialist
 go get -v
 go build -ldflags "-X main.Version=$1" -o /bin/logspout
 apk del go git mercurial build-base
