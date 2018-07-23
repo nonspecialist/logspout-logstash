7d140af3a193f1ca17a9a3c74961b408e44c3bb6
Colin Panisset <colin.panisset@cevo.com.au>
2018-07-23 13:14:22 +1000

    Emit debug during build messages

--- docker/build.sh
+++ docker/build.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 set -e
 apk add --update go git mercurial build-base ca-certificates
-mkdir -p /go/src/github.com/gliderlabs
-cp -r /src /go/src/github.com/gliderlabs/logspout
-cd /go/src/github.com/gliderlabs/logspout
+mkdir -p /go/src/github.com/nonspecialist
+cp -r /src /go/src/github.com/nonspecialist/logspout-logstash-k8s
+cd /go/src/github.com/nonspecialist/logspout-logstash-k8s
 export GOPATH=/go
+echo Building version $1
 go get
 go build -ldflags "-X main.Version=$1" -o /bin/logspout
+echo Cleanup
 apk del go git mercurial build-base
 rm -rf /go
 rm -rf /var/cache/apk/*
