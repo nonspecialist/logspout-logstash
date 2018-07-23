bcf6c389ced5d9510f0a431839394f33eaf5928c
Colin Panisset <colin.panisset@cevo.com.au>
2018-07-23 13:42:16 +1000

    Fixes to build script

--- docker/build.sh
+++ docker/build.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 set -e
 apk add --update go git mercurial build-base ca-certificates
-mkdir -p /go/src/github.com/nonspecialist
-cp -r /src /go/src/github.com/nonspecialist/logspout
-cd /go/src/github.com/nonspecialist/logspout
+mkdir -p /go/src/github.com/gliderlabs
+cp -r /src /go/src/github.com/gliderlabs/logspout
+cd /go/src/github.com/gliderlabs/logspout
 export GOPATH=/go
-echo Building version $1
-go get -v -u
+go get -u -v
 go build -ldflags "-X main.Version=$1" -o /bin/logspout
-echo Cleanup
 apk del go git mercurial build-base
 rm -rf /go
 rm -rf /var/cache/apk/*
