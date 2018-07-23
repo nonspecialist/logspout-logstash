01951e61a1e4a4f4eb6ed9b2c312ab61d417c2ff
Colin Panisset <colin.panisset@cevo.com.au>
2018-07-23 13:59:41 +1000

    workaround cached versions

--- docker/build.sh
+++ docker/build.sh
@@ -5,7 +5,9 @@ mkdir -p /go/src/github.com/gliderlabs
 cp -r /src /go/src/github.com/gliderlabs/logspout
 cd /go/src/github.com/gliderlabs/logspout
 export GOPATH=/go
-go get -u -v
+echo Cleaning up old versions
+rm -rf /go/src/github.com/nonspecialist
+go get -v
 go build -ldflags "-X main.Version=$1" -o /bin/logspout
 apk del go git mercurial build-base
 rm -rf /go
